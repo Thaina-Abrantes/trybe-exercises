a3711665d2b5853a3787e1250d98dc365929051e
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-09-15 19:29:11 -0300

    Ordena array, utiliza arrow function e template literals

--- 01-fundamentos/secao-7-introduco-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/index.js
+++ 01-fundamentos/secao-7-introduco-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/index.js
@@ -26,3 +26,11 @@ const testingScope = (escopo) => {
   }
 
 testingScope(true);
+
+//Crie uma função que retorne um array em ordem crescente.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = oddsAndEvens.sort( (a, b) => a - b);
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
