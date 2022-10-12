6668dd73964fec7b5925fa81e12164963b439114
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-10-11 21:25:08 -0300

    getPokemonDetails

---
+++ 02-front-end/secao-1-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/pokemon.js
@@ -0,0 +1,43 @@
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grama',
+    ability: 'Raio Solar',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fogo',
+    ability: 'Lança Chamas',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Água',
+    ability: 'Jato de Água',
+  },
+];
+
+/**Ao chamar a função getPokemonDetails com um pokémon existente, imprime no console a mensagem com os detalhes deste pokémon.
+Ao chamar a função getPokemonDetails com um pokémon não existente, imprime no console o erro. */
+
+function getPokemonDetails(selectedPokemon, callback) {
+  const foundPokemon = pokemons.find((pokemon) => pokemon.name === selectedPokemon);
+
+  setTimeout(() => {
+    if (foundPokemon === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+
+    const { name, type, ability } = foundPokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
+}
+
+const handlePokemonSearch = (error, message) => {
+  if(error === null)  console.log(message);
+  if (message === null) console.log(error.message);
+};
+
+getPokemonDetails('Squirtle', handlePokemonSearch);
